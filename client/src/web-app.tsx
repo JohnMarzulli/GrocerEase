a9f247679b1745aa7bbcf83ffbc53357ded3d0f5
John Marzulli <john.marzulli@hotmail.com>
2025-10-25 21:38:49 -0700

    Standardize naming

--- client/src/web-app.tsx
+++ client/src/web-app.tsx
@@ -1,6 +1,6 @@
-import { Route, Routes, Navigate } from 'react-router-dom';
-import Home from '@/pages/Home';
-import ListEditor from '@/pages/ListEditor';
+import Home from '@/pages/home';
+import ListEditor from '@/pages/list-editor';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 export default function App() {
   return (
