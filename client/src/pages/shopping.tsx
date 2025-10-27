d21fc41a519ae7b7bbdc92d2ede0464854c695b0
John Marzulli <john.marzulli@hotmail.com>
2025-10-27 02:25:29 -0700

    Gray out items that have been purchased

--- client/src/pages/shopping.tsx
+++ client/src/pages/shopping.tsx
@@ -1,7 +1,7 @@
 import { getValidListIdFromQueryParams, groceryListManager } from '@/core/gocery-list-manager';
 import GroceryList from '@/core/grocery-list';
 import { useRef } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 
 export default function Shopping() {
@@ -13,6 +13,11 @@ export default function Shopping() {
   const list: GroceryList = groceryListManager.getList(listId);
   const listItems = list.getList().items;
 
+  const navigate = useNavigate();
+
+  // Consideration: In the future we will save the GPS location and associate it with the item name
+  //                What if the item is UNCLICKED?
+
   return (
     <div className="mobile-shell">
       <header className="header" style={{ textAlign: 'center', fontSize: 32 }}>
@@ -22,8 +27,16 @@ export default function Shopping() {
         <ul className="list" ref={listRef}>
           {(listItems ?? []).map((i) => (
             <li key={i.id} data-item-id={i.id} style={{ display: 'flex', alignItems: 'center', gap: 12, fontSize: '150%' }}>
-              <input className="interactive-btn" type="checkbox" />
-              {i.name}
+              <input
+                className="interactive-btn"
+                type="checkbox"
+                checked={i.status === 'completed'}
+                onChange={() => {
+                  list.itemAcquired(i.id);
+                  navigate(0);
+                }}
+              />
+              <span className="item-text" style={{ textDecoration: (i.status === 'completed' ? 'line-through' : ''), color: (i.status === 'completed' ? 'gray' : '') }} >{i.name}</span>
               <div style={{ display: 'flex', gap: 6, marginLeft: 'auto', alignItems: 'center' }}>
                 <span className="badge">{i.qty} {i.unit}</span>
               </div>
